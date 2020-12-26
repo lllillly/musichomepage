59f8c9c50c54fac0a92b4095a3683fcd6ef6896f
YERIM <dnaer456@gmail.com>
2020-12-26 20:11:41 +0900

    MM01

--- src/components/MM01.js
+++ src/components/MM01.js
@@ -2,7 +2,11 @@ import React from "react";
 
 class MM01 extends React.Component {
   render() {
-    return <div className="MM01">안녕하세요 여기는 MM01</div>;
+    return (
+      <div className="MM01">
+        <div className="MM01__title">르네상스(1400~1650)</div>
+      </div>
+    );
   }
 }
 
