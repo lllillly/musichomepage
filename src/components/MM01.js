21bb70b0ecf664900bc6e09da9b7f048a9084e5b
YERIM <dnaer456@gmail.com>
2020-12-28 17:42:51 +0900

    MM01

--- src/components/MM01.js
+++ src/components/MM01.js
@@ -5,6 +5,9 @@ class MM01 extends React.Component {
     return (
       <div className="MM01">
         <div className="MM01__title">르네상스(1400~1650)</div>
+        <div className="MM01__desc">
+          <div className="MM01__desc1">주요 작곡가</div>
+        </div>
       </div>
     );
   }
