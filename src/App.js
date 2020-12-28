d924b35d5dd09740e44a77ee37bbbbe66956517c
HJ <user@users-MacBook-Pro.local>
2020-12-28 18:32:20 +0900

    aefjehafafja

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ class App extends React.Component {
             <Route exact path="/mm04" component={MM04} />
             <Route exact path="/mm05" component={MM05} />
             <Route exact path="/mm06" component={MM06} />
-            <Route exact path="/footer" component={Footer} />
           </div>
+          <Footer />
         </Router>
       </div>
     );
