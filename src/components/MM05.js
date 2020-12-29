a68a6fcecb32f550cde9e37e018dff94abc3474c
YERIM <dnaer456@gmail.com>
2020-12-29 11:22:35 +0900

    MM03 end MM05 Start

--- src/components/MM05.js
+++ src/components/MM05.js
@@ -2,7 +2,16 @@ import React from "react";
 
 class MM05 extends React.Component {
   render() {
-    return <div className="MM05">안녕하세요 여기는 MM05</div>;
+    return (
+      <div className="MM05">
+        <div className="MM05__title">인상주의(1870~1915)</div>
+        <div className="MM05__desc">
+          <div className="MM05__composer">주요 작곡가</div>
+          <div className="MM05__composer__introduce"></div>
+        </div>
+        <div className="MM05__point">특징</div>
+      </div>
+    );
   }
 }
 
