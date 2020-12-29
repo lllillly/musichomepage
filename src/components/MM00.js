73f6992dd9693a2e5896dea9be88cb4f9b9bf8e0
YERIM <dnaer456@gmail.com>
2020-12-29 23:25:40 +0900

    MM00

--- src/components/MM00.js
+++ src/components/MM00.js
@@ -1,8 +1,9 @@
 import React from "react";
+import music from "../img/music.png";
 
 class MM00 extends React.Component {
   render() {
-    return <div className="MM00">안녕하세요 여기는 MM00</div>;
+    return <div className="MM00">클래식 음악의 역사 정보</div>;
   }
 }
 
